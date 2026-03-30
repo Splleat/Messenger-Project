cd82697902db75bb2ffbe7cf358b2b03cff751a7
Splleat <Splleat@gmail.com>
2026-03-30 17:09:49 +0900

    feat: JpaAuditing 설정

---
+++ src/main/java/me/splleat/messengerproject/global/config/JpaConfig.java
@@ -0,0 +1,8 @@
+package me.splleat.messengerproject.global.config;
+
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.stereotype.Component;
+
+@EnableJpaAuditing
+@Component
+public class JpaConfig {}

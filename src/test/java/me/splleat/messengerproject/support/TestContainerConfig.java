283b4149a57fbd9a033fcc2d9414b0bcd8f412e0
Splleat <Splleat@gmail.com>
2026-04-13 11:17:19 +0900

    test: TestContainer 설정

---
+++ src/test/java/me/splleat/messengerproject/support/TestContainerConfig.java
@@ -0,0 +1,30 @@
+package me.splleat.messengerproject.support;
+
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.springframework.context.annotation.Bean;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.mysql.MySQLContainer;
+import org.testcontainers.utility.DockerImageName;
+
+@TestConfiguration(proxyBeanMethods = false)
+public class TestContainerConfig {
+
+    @Bean
+    @ServiceConnection(name = "mysql")
+    MySQLContainer mySQLContainer() {
+        return new MySQLContainer(DockerImageName.parse("mysql:9.6.0"))
+                .withDatabaseName("test_db")
+                .withUsername("test")
+                .withPassword("test")
+                .withReuse(true);
+    }
+
+    @Bean
+    @ServiceConnection(name = "redis")
+    GenericContainer<?> redisContainer() {
+        return new GenericContainer<>(DockerImageName.parse("redis:7.4-alpine"))
+                .withExposedPorts(6379)
+                .withReuse(true);
+    }
+}

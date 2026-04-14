d0a153181e479f61fa6ae7664befcc29b40f1918
Splleat <Splleat@gmail.com>
2026-04-14 17:15:21 +0900

    fix: Column 어노테이션 설정

--- src/main/java/me/splleat/messengerproject/infrastructure/persistence/entity/SoftDeletableEntity.java
+++ src/main/java/me/splleat/messengerproject/infrastructure/persistence/entity/SoftDeletableEntity.java
@@ -1,15 +1,22 @@
 package me.splleat.messengerproject.infrastructure.persistence.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class SoftDeletableEntity extends BaseEntity {
+
     @LastModifiedDate
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Column(name = "deleted_at")
     private LocalDateTime deletedAt;
 
     public boolean isDeleted() {

692d677eb5bbc2a62c3ef271d1599ef373c2f5e2
Splleat <Splleat@gmail.com>
2026-04-23 14:27:04 +0900

    feat: 그룹 생성 로직 구현

--- src/main/java/me/splleat/messengerproject/domain/member/GroupMember.java
+++ src/main/java/me/splleat/messengerproject/domain/member/GroupMember.java
@@ -2,8 +2,10 @@ package me.splleat.messengerproject.domain.member;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.Builder;
 import lombok.NoArgsConstructor;
 import me.splleat.messengerproject.domain.group.Group;
+import me.splleat.messengerproject.domain.member.exception.GroupMemberNotPermittedException;
 import me.splleat.messengerproject.domain.user.User;
 import me.splleat.messengerproject.infrastructure.persistence.entity.BaseEntity;
 
@@ -26,4 +28,30 @@ public class GroupMember extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(name = "role")
     private GroupRole role;
+
+    public Long getUserId() {
+        return user.getId();
+    }
+
+    public Long getGroupId() {
+        return group.getId();
+    }
+
+    @Builder
+    private GroupMember(User user, Group group, String nickname, GroupRole role) {
+        this.user = user;
+        this.group = group;
+        this.nickname = nickname;
+        this.role = role;
+    }
+
+    public static GroupMember create(User user, Group group, String nickname, GroupRole role) {
+        return new GroupMember(user, group, nickname, role);
+    }
+
+    public void validatePermission(GroupRole requiredRole) {
+        if (!role.hasPermission(requiredRole)) {
+            throw new GroupMemberNotPermittedException();
+        }
+    }
 }
--- src/main/java/me/splleat/messengerproject/domain/member/GroupRole.java
+++ src/main/java/me/splleat/messengerproject/domain/member/GroupRole.java
@@ -1,6 +1,15 @@
 package me.splleat.messengerproject.domain.member;
 
 public enum GroupRole {
+    OWNER,
     ADMIN,
-    MEMBER
+    MEMBER;
+
+    public boolean hasPermission(GroupRole required) {
+        return switch (required) {
+            case MEMBER -> true;
+            case ADMIN -> this == OWNER || this == ADMIN;
+            case OWNER -> this == OWNER;
+        };
+    }
 }

d78f647d6f3bec4ab411a6cd3433519874ebfdb4
Splleat <Splleat@gmail.com>
2026-05-28 13:45:26 +0900

    refactor: 메시지 저장 멱등성 로직 개선 및 날짜 포맷 설정 추가

--- src/main/java/me/splleat/messengerproject/domain/message/MessageService.java
+++ src/main/java/me/splleat/messengerproject/domain/message/MessageService.java
@@ -4,33 +4,16 @@ import lombok.RequiredArgsConstructor;
 import me.splleat.messengerproject.infrastructure.persistence.jpa.MessageRepository;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
-
-import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class MessageService {
     private final MessageRepository messageRepository;
 
-    @Transactional(propagation = Propagation.REQUIRES_NEW)
+    @Transactional
     public Message registerWithIdempotency(Message message) {
-        try {
-            return messageRepository.save(message);
-        } catch (DataIntegrityViolationException _) {
-            return messageRepository.findByIdemPotencyKey(message.getIdemPotencyKey());
-        }
-    }
-
-    // TODO: 커서 기반 페이징 추가
-    @Transactional(readOnly = true)
-    public List<Message> getChannelMessages(long channelId) {
-        return messageRepository.findAllByChannelId(channelId);
-    }
-
-    @Transactional(readOnly = true)
-    public List<Long> getUserIdsByChannelMessages(long channelId) {
-        return messageRepository.findAllUserIdByChannelId(channelId);
+        return messageRepository.findByIdemPotencyKey(message.getIdemPotencyKey())
+                .orElseGet(() -> messageRepository.save(message));
     }
 }

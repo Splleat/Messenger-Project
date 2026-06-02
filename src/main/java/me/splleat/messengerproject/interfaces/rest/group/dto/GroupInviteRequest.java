d39636f29fe5464889302840bdde1a61b60c3a77
Splleat <Splleat@gmail.com>
2026-06-02 14:47:03 +0900

    feat: 그룹 초대 요청 유효성 검증 추가

--- src/main/java/me/splleat/messengerproject/interfaces/rest/group/dto/GroupInviteRequest.java
+++ src/main/java/me/splleat/messengerproject/interfaces/rest/group/dto/GroupInviteRequest.java
@@ -1,10 +1,12 @@
 package me.splleat.messengerproject.interfaces.rest.group.dto;
 
+import jakarta.validation.constraints.NotEmpty;
 import me.splleat.messengerproject.application.group.dto.GroupInviteCommand;
 
 import java.util.List;
 
 public record GroupInviteRequest(
+        @NotEmpty
         List<Long> targetIds
 ) {
     public GroupInviteCommand toCommand(long userId, long groupId) {

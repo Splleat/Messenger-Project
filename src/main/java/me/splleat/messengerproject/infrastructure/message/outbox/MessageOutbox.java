858ea3161b1dbc54bb096cdc14a799e131e63384
Splleat <Splleat@gmail.com>
2026-06-02 17:28:03 +0900

    feat: 메시지 발행 아웃박스 패턴 적용 및 Redis Pub/Sub 연동 추가

---
+++ src/main/java/me/splleat/messengerproject/infrastructure/message/outbox/MessageOutbox.java
@@ -0,0 +1,52 @@
+package me.splleat.messengerproject.infrastructure.message.outbox;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Lob;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
+import me.splleat.messengerproject.infrastructure.persistence.entity.BaseEntity;
+
+@Entity
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class MessageOutbox extends BaseEntity {
+    @Column(name = "aggregate_id")
+    private long aggregateId;
+
+    @Column(name = "aggregate_type")
+    private String aggregateType;
+
+    @Column(name = "event_type")
+    private String eventType;
+
+    @Column(name = "processed")
+    private boolean processed;
+
+    @Lob
+    @Column(name = "payload")
+    private String payload;
+
+    @Builder
+    private MessageOutbox(long aggregateId, String aggregateType, String eventType, String payload, boolean processed) {
+        this.aggregateId = aggregateId;
+        this.aggregateType = aggregateType;
+        this.eventType = eventType;
+        this.payload = payload;
+        this.processed = processed;
+    }
+
+    public static MessageOutbox create(long aggregateId, String aggregateType, String eventType, String payload) {
+        return MessageOutbox.builder()
+                .aggregateId(aggregateId)
+                .aggregateType(aggregateType)
+                .eventType(eventType)
+                .payload(payload)
+                .processed(false)
+                .build();
+    }
+
+    public void complete() {
+        this.processed = true;
+    }
+}

b321e49f353c69b77037bb8be893453ade8508db
Splleat <Splleat@gmail.com>
2026-06-12 16:41:11 +0900

    refactor: 가상 스레드 활성화 및 아웃박스 이벤트 비동기 처리

--- src/main/java/me/splleat/messengerproject/infrastructure/message/outbox/MessageOutboxEventListener.java
+++ src/main/java/me/splleat/messengerproject/infrastructure/message/outbox/MessageOutboxEventListener.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.splleat.messengerproject.infrastructure.message.publisher.MessagePublisher;
 import org.springframework.context.event.EventListener;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.event.TransactionPhase;
 import org.springframework.transaction.event.TransactionalEventListener;
@@ -22,6 +23,7 @@ public class MessageOutboxEventListener {
         messageOutboxService.saveOutbox(event.toOutbox(jsonMapper));
     }
 
+    @Async
     @TransactionalEventListener(phase = TransactionPhase.AFTER_COMMIT)
     public void publishToRedis(MessageCreateEvent event) {
         try {

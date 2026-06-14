1d28c941a0319d661c226f02597b18bf6028e608
Splleat <Splleat@gmail.com>
2026-06-14 14:45:43 +0900

    refactor: MessageOutboxRepository 패키지 이동 및 미처리 아웃박스 조회용 인덱스, 쿼리 추가

--- src/main/java/me/splleat/messengerproject/infrastructure/message/outbox/MessageOutboxRepository.java
+++
@@ -1,9 +0,0 @@
-package me.splleat.messengerproject.infrastructure.message.outbox;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.Optional;
-
-public interface MessageOutboxRepository extends JpaRepository<MessageOutbox, Long> {
-    Optional<MessageOutbox> findByMessageIdAndProcessedFalse(long messageId);
-}

334e2db6017f954260294b079bf81b438be8b0e1
Splleat <Splleat@gmail.com>
2026-06-17 13:47:10 +0900

    feat: Attachment 도메인 모델 강화

--- src/main/java/me/splleat/messengerproject/domain/message/AttachmentService.java
+++ src/main/java/me/splleat/messengerproject/domain/message/AttachmentService.java
@@ -1,47 +1,24 @@
 package me.splleat.messengerproject.domain.message;
 
-import me.splleat.messengerproject.interfaces.rest.attachment.dto.PresignRequest;
-import me.splleat.messengerproject.interfaces.rest.attachment.dto.PresignResponse;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import me.splleat.messengerproject.infrastructure.persistence.jpa.AttachmentRepository;
 import org.springframework.stereotype.Service;
-import software.amazon.awssdk.services.s3.model.PutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.S3Presigner;
-import software.amazon.awssdk.services.s3.presigner.model.PresignedPutObjectRequest;
-import software.amazon.awssdk.services.s3.presigner.model.PutObjectPresignRequest;
+import org.springframework.transaction.annotation.Transactional;
 
-import java.time.Duration;
-import java.util.UUID;
+import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class AttachmentService {
-    private final S3Presigner s3Presigner;
-    private final String bucket;
+    private final AttachmentRepository attachmentRepository;
 
-    private static final String KEY = "attachments/";
-
-    @Autowired
-    public AttachmentService(S3Presigner s3Presigner, @Value("${minio.bucket}") String bucket) {
-        this.s3Presigner = s3Presigner;
-        this.bucket = bucket;
+    @Transactional
+    public void registerAll(List<Attachment> attachments) {
+        attachmentRepository.saveAll(attachments);
     }
 
-    public PresignResponse createPresignedUpload(PresignRequest request) {
-        String objectKey = KEY + UUID.randomUUID();
-
-        PutObjectRequest putRequest = PutObjectRequest.builder()
-                .bucket(bucket)
-                .key(objectKey)
-                .contentType(request.contentType())
-                .build();
-
-        PresignedPutObjectRequest presigned = s3Presigner.presignPutObject(
-                PutObjectPresignRequest.builder()
-                        .signatureDuration(Duration.ofMinutes(5))
-                        .putObjectRequest(putRequest)
-                        .build()
-        );
-
-        return new PresignResponse(presigned.url().toString(), objectKey);
+    @Transactional(readOnly = true)
+    public List<Attachment> getAttachments(long messageId) {
+        return attachmentRepository.findByMessageId(messageId);
     }
 }
--- src/main/java/me/splleat/messengerproject/domain/message/AttachmentType.java
+++ src/main/java/me/splleat/messengerproject/domain/message/AttachmentType.java
@@ -1,6 +1,25 @@
 package me.splleat.messengerproject.domain.message;
 
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import me.splleat.messengerproject.common.exception.BusinessException;
+import me.splleat.messengerproject.common.exception.ErrorCode;
+
+import java.util.Arrays;
+import java.util.Objects;
+
+@Getter
+@RequiredArgsConstructor
 public enum AttachmentType {
-    IMAGE,
-    FILE
+    IMAGE("image"),
+    FILE("file");
+
+    private final String value;
+
+    public static AttachmentType from(String value) {
+        return Arrays.stream(AttachmentType.values())
+                .filter(type -> Objects.equals(type.value, value))
+                .findFirst()
+                .orElseThrow(() -> new BusinessException(ErrorCode.ATTACHMENT_UNSUPPORTED_TYPE));
+    }
 }

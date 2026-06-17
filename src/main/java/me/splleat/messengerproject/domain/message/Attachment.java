a2eb88c9b3cbf07123bbb5d3470c5594bfcc4149
Splleat <Splleat@gmail.com>
2026-06-17 13:47:10 +0900

    feat: Attachment 도메인 모델 강화

--- src/main/java/me/splleat/messengerproject/domain/message/Attachment.java
+++ src/main/java/me/splleat/messengerproject/domain/message/Attachment.java
@@ -2,6 +2,7 @@ package me.splleat.messengerproject.domain.message;
 
 import jakarta.persistence.*;
 import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import me.splleat.messengerproject.infrastructure.persistence.entity.SoftDeletableEntity;
@@ -10,6 +11,7 @@ import me.splleat.messengerproject.infrastructure.persistence.entity.SoftDeletab
 @Table(name = "attachments")
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class Attachment extends SoftDeletableEntity {
 
     @Column(name = "message_id")
@@ -21,4 +23,8 @@ public class Attachment extends SoftDeletableEntity {
 
     @Column(name = "url")
     private String url;
+
+    public static Attachment create(long messageId, AttachmentType type, String url) {
+        return new Attachment(messageId, type, url);
+    }
 }

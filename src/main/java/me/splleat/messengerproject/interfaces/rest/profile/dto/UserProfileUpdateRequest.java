3ff095764084641961fa3deee5e880b2a562b507
Splleat <Splleat@gmail.com>
2026-06-23 16:26:55 +0900

    refactor: 프로필 이름 유효성 검사 추가 및 문자열 공백 검증 개선

--- src/main/java/me/splleat/messengerproject/interfaces/rest/profile/dto/UserProfileUpdateRequest.java
+++ src/main/java/me/splleat/messengerproject/interfaces/rest/profile/dto/UserProfileUpdateRequest.java
@@ -1,9 +1,12 @@
 package me.splleat.messengerproject.interfaces.rest.profile.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import me.splleat.messengerproject.application.profile.dto.UserProfileUpdateCommand;
 
 public record UserProfileUpdateRequest(
+        @NotBlank
         String name,
+
         String statusMessage
 ) {
     public UserProfileUpdateCommand toCommand(long userId) {

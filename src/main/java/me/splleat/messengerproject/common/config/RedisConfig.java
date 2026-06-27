e8955033f37b9445e0ea39b6de562777aa8092bb
Splleat <splleat@gmail.com>
2026-06-27 15:15:42 +0900

    fix: RedisConnectionFactory 생성 AutoConfiguration에 위임

--- src/main/java/me/splleat/messengerproject/common/config/RedisConfig.java
+++ src/main/java/me/splleat/messengerproject/common/config/RedisConfig.java
@@ -1,13 +1,9 @@
 package me.splleat.messengerproject.common.config;
 
-import lombok.RequiredArgsConstructor;
 import me.splleat.messengerproject.infrastructure.message.subscriber.RedisSubscriber;
-import org.springframework.boot.data.redis.autoconfigure.DataRedisProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
-import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.ChannelTopic;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
@@ -16,22 +12,13 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 @EnableRedisRepositories
-@RequiredArgsConstructor
 public class RedisConfig {
 
-    private final DataRedisProperties redisProperties;
     private static final String TOPIC = "messenger:channel";
 
     @Bean
     ChannelTopic channelTopic() {
         return new ChannelTopic(TOPIC);
-    }
-
-    @Bean
-    RedisConnectionFactory redisConnectionFactory() {
-        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration(redisProperties.getHost(), redisProperties.getPort());
-
-        return new LettuceConnectionFactory(config);
     }
 
     @Bean

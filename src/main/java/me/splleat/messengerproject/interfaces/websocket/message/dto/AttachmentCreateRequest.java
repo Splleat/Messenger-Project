51bb42cc6d5f862872b20122282f85de9e29402f
Splleat <splleat@gmail.com>
2026-07-03 21:49:57 +0900

    feat: Attachment 스키마 변경 (originalName, size 추가)

--- src/main/java/me/splleat/messengerproject/interfaces/websocket/message/dto/AttachmentCreateRequest.java
+++ src/main/java/me/splleat/messengerproject/interfaces/websocket/message/dto/AttachmentCreateRequest.java
@@ -4,10 +4,12 @@ import me.splleat.messengerproject.domain.message.Attachment;
 import me.splleat.messengerproject.domain.message.AttachmentType;
 
 public record AttachmentCreateRequest(
+        String name,
         String type,
-        String url
+        String url,
+        long size
 ) {
     public Attachment toEntity(long messageId) {
-        return Attachment.create(messageId, AttachmentType.from(type), url);
+        return Attachment.create(messageId, name, AttachmentType.from(type), url, size);
     }
 }

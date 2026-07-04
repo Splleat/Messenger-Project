c0ac5f8897a99d394e5ee615a1967941c4ab77fc
Splleat <splleat@gmail.com>
2026-07-04 22:07:26 +0900

    fix: 메시지 업데이트 DTO 필드 변경(payload -> content)

--- src/main/java/me/splleat/messengerproject/interfaces/websocket/message/dto/MessageUpdateRequest.java
+++ src/main/java/me/splleat/messengerproject/interfaces/websocket/message/dto/MessageUpdateRequest.java
@@ -3,9 +3,9 @@ package me.splleat.messengerproject.interfaces.websocket.message.dto;
 import me.splleat.messengerproject.application.message.dto.MessageUpdateCommand;
 
 public record MessageUpdateRequest(
-        String payload
+        String content
 ) {
     public MessageUpdateCommand toCommand(long userId, long messageId) {
-        return new MessageUpdateCommand(userId, messageId, payload);
+        return new MessageUpdateCommand(userId, messageId, content);
     }
 }
